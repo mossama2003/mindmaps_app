ce5bdf043c893a2fa4adb90f19e2757b48bf7d5f
Mohamed Osama <mossama370@gmail.com>
2025-07-22 01:46:24 +0300

    updatedScreen

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -30,8 +30,6 @@ class AppAssetsSvg {
       '$svg/sign_up_background_shape.svg';
   static final String signUpBackgroundShape2 =
       '$svg/sign_up_background_shape2.svg';
-  static final String google = '$svg/google.svg';
-  static final String apple = '$svg/apple.svg';
 
   static final String chatBot = '$svg/chat_bot.svg';
   static final String uploadImage = '$svg/upload_image.svg';
--- lib/features/parent_sign_up/presentation/pages/parent_sign_up_screen.dart
+++ lib/features/parent_sign_up/presentation/pages/parent_sign_up_screen.dart
@@ -1,13 +1,16 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mindmaps_app/core/constants/app_constants.dart';
+import 'package:mindmaps_app/core/widgets/custom_button.dart';
+import 'package:mindmaps_app/core/widgets/custom_icon.dart';
+
 import '../../../../core/constants/app_assets.dart';
 import '../../../../core/helper/app_navigator.dart';
 import '../../../../core/style/app_colors.dart';
+import '../../../../core/style/app_size.dart';
 import '../../../../core/widgets/custom_field_text.dart';
 import '../../../parent/sign_in/presentation/screens/parent_sign_in_screen.dart';
-import 'add_child_screen.dart';
 
 class ParentSignUpScreen extends StatelessWidget {
   const ParentSignUpScreen({super.key});
@@ -15,7 +18,6 @@ class ParentSignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.white,
       body: SafeArea(
         child: SizedBox(
           height: double.infinity,
@@ -26,7 +28,7 @@ class ParentSignUpScreen extends StatelessWidget {
                 left: 0,
                 child: SvgPicture.asset(
                   AppAssetsSvg.signUpBackgroundShape,
-                  width: 80.w,
+                  width: AppSize.getWidth(80),
                 ),
               ),
               Positioned(
@@ -34,59 +36,61 @@ class ParentSignUpScreen extends StatelessWidget {
                 right: 0,
                 child: SvgPicture.asset(
                   AppAssetsSvg.signUpBackgroundShape2,
-                  width: 80.w,
+                  width: AppSize.getWidth(80),
                 ),
               ),
 
-              
               SingleChildScrollView(
-                padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 16.h),
+                padding: AppSize.padding(horizontal: 24, vertical: 16),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    SizedBox(height: 5.h),
+                    SizedBox(height: AppSize.getHeight(5)),
 
                     Center(
                       child: Container(
-                        width: 60.w,
-                        height: 60.h,
+                        width: AppSize.getSize(60),
+                        height: AppSize.getSize(60),
                         decoration: BoxDecoration(
                           shape: BoxShape.circle,
                           color: AppColors.grey20,
                         ),
                         child: Padding(
-                          padding: EdgeInsets.all(16.w),
-                          child: SvgPicture.asset(
-                            AppAssetsSvg.logo,
-                            fit: BoxFit.contain,
+                          padding: AppSize.padding(all: 16),
+                          child: CustomIcon(
+                            icon: AppAssetsSvg.logo,
+                            withColor: true,
                           ),
                         ),
                       ),
                     ),
-                    SizedBox(height: 5.h),
+                    SizedBox(height: AppSize.getHeight(5)),
 
                     /// Title
                     Text(
                       'sign_up.parent_sign_up'.tr(),
                       style: TextStyle(
-                        fontSize: 20.sp,
-                        fontWeight: FontWeight.bold,
+                        fontSize: AppSize.font(20),
+                        fontFamily: AppConstants.tajawalFont,
+                        fontWeight: FontWeight.w700,
                         color: AppColors.black,
                       ),
                     ),
-                    SizedBox(height: 3.h),
+                    SizedBox(height: AppSize.getHeight(6.5)),
 
                     /// Subtitle
                     Text(
                       'sign_up.subtitle_parent_sign_up'.tr(),
                       textAlign: TextAlign.center,
                       style: TextStyle(
-                        fontSize: 14.sp,
+                        fontSize: AppSize.font(14),
+                        fontFamily: AppConstants.tajawalFont,
+                        fontWeight: FontWeight.w700,
                         color: AppColors.black100,
                       ),
                     ),
-                    SizedBox(height: 24.h),
+                    SizedBox(height: AppSize.getHeight(24)),
 
                     CustomFieldText(
                       title: "sign_up.full_name".tr(),
@@ -94,7 +98,7 @@ class ParentSignUpScreen extends StatelessWidget {
                       hintText: "sign_up.enter_name".tr(),
                       iconStart: AppAssetsIcons.personIcon,
                     ),
-                    SizedBox(height: 12.h),
+                    SizedBox(height: AppSize.getHeight(16)),
 
                     CustomFieldText(
                       title: 'sign_in.phone_number'.tr(),
@@ -103,7 +107,7 @@ class ParentSignUpScreen extends StatelessWidget {
                       iconStart: AppAssetsIcons.phone,
                       keyboardType: TextInputType.phone,
                     ),
-                    SizedBox(height: 12.h),
+                    SizedBox(height: AppSize.getHeight(16)),
 
                     CustomFieldText(
                       title: 'sign_in.password'.tr(),
@@ -111,74 +115,62 @@ class ParentSignUpScreen extends StatelessWidget {
                       controller: TextEditingController(),
                       iconStart: AppAssetsIcons.password,
                       iconEnd: AppAssetsIcons.openEye,
-                      // iconEnd: _obscurePassword ? 'ic_eye_hide' : 'ic_eye',
-                      // obscureText: _obscurePassword,
-                      // iconEndTap: () {
-                      //   setState(() {
-                      //     _obscurePassword = !_obscurePassword;
-                      //   });
-                      // },
-                      // divider: true,
                     ),
-                    SizedBox(height: 24.h),
+                    SizedBox(height: AppSize.getHeight(47)),
 
                     /// Next Button
-                    SizedBox(
-                      width: double.infinity,
-                      height: 48.h,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          AppNavigator.push(AddChildScreen());
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: AppColors.primary,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.r),
-                          ),
-                        ),
-                        child: Text(
-                          'sign_up.next'.tr(),
-                          style: TextStyle(
-                            fontSize: 16.sp,
-                            fontWeight: FontWeight.bold,
-                            color: AppColors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 15.h),
-
+                    CustomButton(title: 'sign_up.next'.tr(), onTap: () {}),
+                    SizedBox(height: AppSize.getHeight(8)),
                     Text(
                       'sign_in.or'.tr(),
-                      style: TextStyle(fontSize: 15.sp, color: AppColors.black),
+                      style: TextStyle(
+                        fontSize: AppSize.font(14),
+                        fontFamily: AppConstants.tajawalFont,
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.black100,
+                      ),
                     ),
-                    SizedBox(height: 10.h),
+                    SizedBox(height: AppSize.getHeight(12)),
 
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        SvgPicture.asset(AppAssetsSvg.apple, width: 32.w),
-                        SizedBox(width: 16.w),
-                        SvgPicture.asset(AppAssetsSvg.google, width: 32.w),
+                        CustomIcon(
+                          icon: AppAssetsIcons.apple,
+                          withColor: true,
+                          width: AppSize.getWidth(25),
+                          height: AppSize.getHeight(25),
+                        ),
+                        SizedBox(width: AppSize.getWidth(16)),
+                        CustomIcon(
+                          icon: AppAssetsIcons.google,
+                          withColor: true,
+                          width: AppSize.getWidth(25),
+                          height: AppSize.getHeight(25),
+                        ),
                       ],
                     ),
-                    SizedBox(height: 20.h),
+                    SizedBox(height: AppSize.getHeight(20)),
 
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text(
                           "sign_up.already_have_account".tr(),
-                          style: TextStyle(fontSize: 14.sp),
+                          style: TextStyle(
+                            fontSize: AppSize.font(14),
+                            fontFamily: AppConstants.tajawalFont,
+                          ),
                         ),
-                        GestureDetector(
+                        InkWell(
                           onTap: () {
-AppNavigator.replace(ParentSignInScreen());
+                            AppNavigator.replace(ParentSignInScreen());
                           },
                           child: Text(
                             "sign_up.login".tr(),
                             style: TextStyle(
-                              fontSize: 14.sp,
+                              fontFamily: AppConstants.tajawalFont,
+                              fontSize: AppSize.font(14),
                               fontWeight: FontWeight.bold,
                               color: AppColors.primary,
                             ),
@@ -186,7 +178,6 @@ AppNavigator.replace(ParentSignInScreen());
                         ),
                       ],
                     ),
-                    SizedBox(height: 20.h),
                   ],
                 ),
               ),
